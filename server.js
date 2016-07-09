77283af93af5d63c56741a84ad05eb815f671a9e
yacaFx <sergiobritor@gmail.com>
2016-07-08 21:57:29 -0500

    fixing port

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname) + '/index.html');
 })
 
-app.listen(80);
+app.set('port', (process.env.PORT || 5000))
+
+app.listen(app.get('port'));
+
 console.log('Here we go!');
