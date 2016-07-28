ef8e54b847e77e856afb61dd7f94cc083e5a585b
yacaFx <sergiobritor@gmail.com>
2016-07-27 21:45:32 -0500

    Files for class 10 -  Angula filters, directives & services

--- angular_sample/js/controller.js
+++ angular_sample/js/controller.js
@@ -1,10 +1,16 @@
 angular.module('myApp.controllers', [])
-    .controller('mainCtrl', function($scope, $filter) {
+    .controller('mainCtrl', function($scope, $filter, cityService) {
         $scope.message = 'La aplicación ha sido creada';
 
         $scope.name = "Sergio Brito";
 
         $scope.toLowerCase = $filter('lowercase')($scope.name);
+
+        $scope.isCapitalized = function(str){
+          return str[0] == str[0].toUpperCase()
+        }
+
+        console.log(cityService.getCity('SFO'))
 
         console.log($scope.message);
     })
@@ -25,4 +31,10 @@ angular.module('myApp.controllers', [])
             //updateClock();
             $scope.$apply(updateClock)
         }, 1000);
+    })
+    .controller('citiesCtrl', function($scope, cityService) {
+      $scope.cities = cityService.getCities();
+      $scope.searchCity = function(cityAbbr){
+        $scope.city = cityService.getCity(cityAbbr)[0];
+      }
     })
